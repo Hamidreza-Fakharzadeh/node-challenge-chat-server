d19e48f3e2629b5118a46d437c7347ddadfb1603
hamidreza <hrfarzadeh@gmail.com>
2020-07-07 19:18:42 +0100

    post method

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser")
 
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(bodyParser.json());
 
 const welcomeMessage = {
   id: 0,
@@ -16,6 +19,7 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
+let idGenerator = 0;
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
@@ -25,6 +29,15 @@ app.get("/messages", function (request, response) {
   response.json(messages);
 });
 
+app.post("/messages", (request, response) => {
+  request.body.from === "" || request.body.text === ""
+  ? response.sendStatus(404).json({"message not send": true})
+  : messages.push({
+    id: idGenerator+=1,
+    from: request.body.from,
+    text: request.body.text
+  })
+})
 app.listen(process.env.PORT || 5000);
 console.log("hi")
 
